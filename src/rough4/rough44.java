ed7e281a4c8ebec73456f014d457cc35a909847e
767461 <767461@LTIN210887.cts.com>
2022-01-17 11:19:01 +0530

    my second commit

---
+++ src/rough4/rough44.java
@@ -0,0 +1,13 @@
+package rough4;
+
+public class rough44 {
+
+	public static void main(String[] args) {
+		String s1="Indhu";
+		String s2="mathi";
+		String s3=s1.concat(s2);
+		System.out.println(s3);
+
+	}
+
+}
